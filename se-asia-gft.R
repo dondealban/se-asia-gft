4adefb6546c073497062b31d518955098726ea0c
Don De Alban <dondealban@gmail.com>
2022-05-26 14:27:48 +0800

    Initial commit of script, incl header comment section

---
+++ se-asia-gft.R
@@ -0,0 +1,13 @@
+# This script generates plots of the results of Southeast Asia's Great Forest
+# Transition study, which estimates the area, total carbon, avoided emissions
+# of undeveloped or unconverted forests within concessions (logging/timber, oil
+# palm, pulp & paper, rubber, mixed) in Southeast Asian countries.
+# 
+# 
+# Script by:      Jose Don T. De Alban
+# Date created:   26 May 2022
+# Date modified:     
+
+
+# Load Libraries -------------------------
+library(tidyverse)
